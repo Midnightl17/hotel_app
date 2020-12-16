6a832ee7839e1b391b096f6ab2624505d856b115
Daniel Fabra <daniel_fabra@outlook.com>
2020-12-16 09:59:57 -0500

    Agregue estilo al UI de registro aplicando Bootstrap.

--- src/components/registro_de_usario.vue
+++ src/components/registro_de_usario.vue
@@ -1,55 +1,130 @@
 <template>
   <div>
-    <img
-      src="https://i.pinimg.com/564x/de/f7/b6/def7b694904830d5804ee5975b69e9ed.jpg"
-      alt=""
-    />
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 col-xl-9 mx-auto">
+          <div class="card card-signin flex-row my-5">
+            <div class="card-img-left d-none d-md-flex">
+              <!-- img de fondo-->
+            </div>
+            <div class="card-body">
+              <h5 class="card-title text-center">Registro de usuario</h5>
+              <form class="form-signin">
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="inputUserame"
+                    class="form-control"
+                    placeholder="Username"
+                    required
+                    autofocus
+                  />
+                  <label for="inputUserame">Username</label>
+                </div>
 
-    <form action="/my-handling-form-page" method="post">
-      <ul>
-        <div style="width: 70%">
-          <label for="mail">Correo electrónico:</label>
-          <input type="email" id="mail" name="user_mail" />
-        </div>
-        <br />
-        <label for="name">Nombre:</label>
-        <input type="text" id="name" name="user_name" />
-        <br />
-        <br />
-        <label for="apellido">Apellido:</label>
-        <input type="text" id="apellido" name="user_apellido" />
-        <br />
-        <br />
-        <label for="edad">Edad:</label>
-        <input type="number" id="edad" name="user_edad" />
-        <br />
-        <br />
-        <label for="contraseña">Contraseña:</label>
-        <input type="password" id="contraseña" name="user_contraseña" />
-        <br />
-        <br />
-        <label for="telefono">Telefono:</label>
-        <input type="number" id="telefono" name="user_telefono" />
-        <br />
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="inputName"
+                    class="form-control"
+                    placeholder="Name user"
+                    required
+                  />
+                  <label for="inputName">Nombres</label>
+                </div>
 
-        <br />
-        <label for="direccion">Direccion:</label>
-        <input type="text" id="direccion" name="user_direccion" />
-        <br />
-        <div>
-          <label for="idioma">Idioma:</label>
-          <select name="my_html_select_box">
-            <option>Español</option>
-            <option>Inglés</option>
-            <option>Portugués</option>
-            <option>Francés</option>
-          </select>
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="inputLastname"
+                    class="form-control"
+                    placeholder="Lastname user"
+                    required
+                  />
+                  <label for="inputLastname">Apellidos</label>
+                </div>
+
+                <div class="form-label-group">
+                  <input
+                    type="email"
+                    id="inputEmail"
+                    class="form-control"
+                    placeholder="Email address"
+                    required
+                  />
+                  <label for="inputEmail">Correo electronico</label>
+                </div>
+
+                <hr />
+
+                <div class="form-label-group">
+                  <input
+                    type="tel"
+                    id="inputTel"
+                    class="form-control"
+                    placeholder="000-000-0000"
+                    required
+                  />
+                  <label for="inputTel">Numero cel/tel</label>
+                </div>
+
+                <div class="form-label-group">
+                  <input
+                    type="number"
+                    id="inputEdad"
+                    class="form-control"
+                    placeholder="Edad"
+                    required
+                  />
+                  <label for="inputEdad">Edad</label>
+                </div>
+                
+                <div class="form-label-group">
+                    <select class="form-control slidioma" id="idiomas">
+                      <option>Español</option>
+                      <option>Inglés</option>
+                      <option>Portugués</option>
+                      <option>Francés</option>
+                    </select>
+                </div>
+                
+                <hr />
+
+                <div class="form-label-group">
+                  <input
+                    type="password"
+                    id="inputPassword"
+                    class="form-control"
+                    placeholder="Password"
+                    required
+                  />
+                  <label for="inputPassword">Contraseña</label>
+                </div>
+
+                <div class="form-label-group">
+                  <input
+                    type="password"
+                    id="inputConfirmPassword"
+                    class="form-control"
+                    placeholder="Password"
+                    required
+                  />
+                  <label for="inputConfirmPassword">Confirmar contraseña</label>
+                </div>
+
+                <button
+                  class="btn btn-lg btn-primary btn-block text-uppercase"
+                  type="submit"
+                >
+                  Registrar
+                </button>
+                <a class="d-block text-center mt-2 small" href="#">Ingresar</a>
+              </form>
+            </div>
+          </div>
         </div>
-        <li class="button">
-          <button type="submit">Registrarse</button>
-        </li>
-      </ul>
-    </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -58,54 +133,118 @@ export default {};
 </script>
 
 <style>
-img {
-  padding-left: 800px;
+:root {
+  --input-padding-x: 1.5rem;
+  --input-padding-y: 0.75rem;
 }
 
-form {
-  /* Centrar el formulario en la página */
-  margin: 0 auto;
-  width: 600px;
-  /* Esquema del formulario */
-  padding: 1em;
-  border: 1px solid #ccc;
-  border-radius: 1em;
+body {
+  background: #023047;
+  background: linear-gradient(to right, #caf0f8, #00b4d8);
 }
 
-ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
+.card-signin {
+  border: 0;
+  border-radius: 0.5rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+  overflow: hidden;
 }
 
-label {
-  /* Tamaño y alineación uniforme */
-  display: inline-block;
-  width: 200px;
-  text-align: right;
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
 }
 
-input,
-textarea {
-  /* Para asegurarse de que todos los campos de texto tienen la misma configuración de letra
-     Por defecto, las áreas de texto tienen un tipo de letra monoespaciada */
-  font: 1em sans-serif;
-
-  /* Tamaño uniforme del campo de texto */
-  width: 200px;
-  box-sizing: border-box;
-
-  /* Hacer coincidir los bordes del campo del formulario */
-  border: 1px solid #999;
-}
-.button {
-  /* Alinear los botones con los campos de texto */
-  padding-left: 90px; /* mismo tamaño que los elementos de la etiqueta */
+.card-signin .card-img-left {
+  width: 45%;
+  /* Link to your background image using in the property below! */
+  background: scroll center
+    url("https://www.hotelresb2b.com/images/hoteles/896798_foto_2.jpg");
+  background-size: cover;
 }
 
-button {
-  /* Este margen adicional representa aproximadamente el mismo espacio que el espacio
-     entre las etiquetas y sus campos de texto */
-  margin-left: 12.5em;
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: 0.1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  height: auto;
+  border-radius: 0.8rem;
+}
+
+.slidioma {
+  height: auto;
+  border-radius: 0.8rem;
+}
+
+.form-label-group > input,
+.form-label-group > label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group > label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  transition: all 0.1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown) ~ label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
 }
 </style>
